77c30e2b9d30d7b8d0adfdbd73a88731e54d211c
Maria Clara <melo.clara@escolar.ifrn.edu.br>
2024-12-05 13:53:09 -0300

    .

--- Circulo/src/Circulo.java
+++ Circulo/src/Circulo.java
@@ -1,16 +1,24 @@
+
 public class Circulo {
     private double raio;
-    public Circulo (double raio){
-        this.raio = raio;
 
+    public Circulo(double raio){
+        this.setRaio(raio);
+    } 
+    public double getRaio(){
+        return this.raio;
     }
-    public double get_area(){
-        return raio * 3.14 * raio;
-        
+
+    public void setRaio(double raio){
+        this.raio = raio;
     }
-    public double get_circunferencia(){
-        return 2 * 3.14 * raio;
-        
+
+    public double getArea(){
+        return Math.PI*Math.pow(this.raio,2);    
+    }
+
+    public double getCircunferencia(){
+        return 2*Math.PI*this.raio;  
     }
 
 }
--- Circulo/src/Main.java
+++ Circulo/src/Main.java
@@ -1,7 +1,13 @@
+
+
 public class Main {
     public static void main(String[] args) {
+
         Circulo circulo = new Circulo(4);
-        System.out.println(circulo.get_area());
-        System.out.println(circulo.get_circunferencia());
+        System.out.printf("Raio: %.2f\n", circulo.getRaio());
+        System.out.printf("Área: %.2f\n", circulo.getArea());
+        System.out.printf("Cicunferência: %.2f\n", circulo.getCircunferencia());
     }
 }
+
+       
